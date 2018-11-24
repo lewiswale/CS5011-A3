f89259ab7714757abbc8758bc93f6f879ba1b9d3
ljw26 <ljw26@st-andrews.ac.uk>
2018-11-24 16:27:01 +0000

    Implemented part 2. Part 1 now saves trained classifier. Part 2 can load model and take user entered answers to questions to guess country.

--- part1/part1.py
+++ part1/part1.py
@@ -2,43 +2,56 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.externals import joblib
 import pandas as pd
+import sys
 
+if len(sys.argv) == 2:
+    print("Using file " + sys.argv[1])
+    file_to_use = '../' + sys.argv[1]
+else:
+    print("Please add training file as argument.")
+    sys.exit(1)
+
+print("Creating classifier...")
 clf = MLPClassifier(solver='sgd', learning_rate_init=0.5, hidden_layer_sizes=(5,), verbose=True, momentum=0.3,
                     activation='logistic', n_iter_no_change=5000, max_iter=10000)
+print(clf)
 
-file_to_use = '../trip2.csv'
-
+print("\nReading file.")
 full_file = pd.read_csv(file_to_use)
 
 column_count = len(full_file.columns)
-print(column_count)
 
 X_cols = [i for i in range(0, column_count-1)]
 
 X = pd.read_csv(file_to_use, usecols=X_cols)
 df_countries = pd.read_csv(file_to_use, usecols=[column_count-1])
 
-print(X)
-print(df_countries)
-
+# print(X)
+# print(df_countries)
+print("\nEncoding data...")
 X = X.replace(regex={r'Yes': 1, r'No': 0})
 
-print(X)
-
-unique_countries = df_countries['Dream Location'].unique()
+# print(X)
 
 enc = OneHotEncoder(handle_unknown='ignore')
-encoded_countries = enc.fit_transform(df_countries)
+enc = enc.fit(df_countries)
+encoded_countries = enc.transform(df_countries).toarray()
+
+# print(encoded_countries)
+print("Data encoded.")
 
 X_train, X_test = X[:50], X[50:]
 y_train, y_test = encoded_countries[:50], encoded_countries[50:]
 
-
+print("\nTraining...")
 clf.fit(X_train.as_matrix(), y_train)
 
-# joblib.dump(clf, 'travel_genie.joblib')
-
-print('TESTING')
+print('\nTESTING')
 print("Training score: %f" % clf.score(X_train.as_matrix(), y_train))
 print("Testing score: %f" % clf.score(X_test.as_matrix(), y_test))
+
+model_name = sys.argv[1][:-4]
+print("\nSaving model as " + model_name + ".joblib")
+joblib.dump(clf, model_name + '.joblib')
+print("Model saved.")
 
---
+++ part2/part2.py
@@ -0,0 +1,99 @@
+import sys
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.externals import joblib
+
+
+def ask_user():
+    if len(sys.argv) == 2:
+        file_to_use = str("../" + sys.argv[1])
+        print("File to use: " + file_to_use)
+
+        df = pd.read_csv(file_to_use)
+
+        # print(df)
+    else:
+        print("Please give a file.")
+        sys.exit(1)
+
+    clf = MLPClassifier()
+
+    if file_to_use == "../trip.csv":
+        clf = joblib.load("../part1/trip.joblib")
+    elif file_to_use == "../trip2.csv":
+        clf = joblib.load("../part1/trip2.joblib")
+
+    # print(clf)
+
+    enc = OneHotEncoder(handle_unknown='ignore')
+    column_count = len(df.columns)
+    df_countries = pd.read_csv(file_to_use, usecols=[column_count-1])
+    enc.fit_transform(df_countries).toarray()
+
+    answers = []
+
+    # print(df.columns)
+
+    for column in df.columns:
+        if column == "Short Stay":
+            print("Would you only be staying for a short amount of time?")
+        elif column == "Penguins":
+            print("Is your dream location inhabited by penguins?")
+        elif column == "Longest rivers":
+            print("Does your dream location contain some of the worlds longest rivers?")
+        elif column == "Island":
+            print("Is your dream destination an island?")
+        elif column == "Seaside":
+            print("Is your dream destination known for its beaches?")
+        elif column == "Historical":
+            print("Is your dream location known for its rich history?")
+        elif column == "Speaking Spanish":
+            print("Is the national language of your dream location Spanish?")
+        elif column == "Food":
+            print("Is your dream destination known for its exotic cuisine?")
+        elif column == "In Europe":
+            print("Is your dream destination located within Europe?")
+        else:
+            continue
+
+        answer = ""
+        while answer != "y" and answer != "Y" and answer != "n" and answer != "N":
+            answer = input("Please answer Y for yes, N for no: ")
+            if answer == "Y" or answer == "y":
+                answers.append(1)
+            elif answer == "N" or answer == "n":
+                answers.append(0)
+
+        # print(answers)
+        if column == "Seaside":
+            make_guess(answers, enc, clf, column_count)
+
+    make_guess(answers, enc, clf, column_count)
+
+
+def make_guess(answers, enc, clf, column_count):
+    current_answers = answers.copy()
+    diff = column_count - 1 - len(current_answers)
+
+    if diff > 0:
+        for i in range(diff):
+            current_answers.append(0)
+
+    to_predict = [current_answers]
+    prediction = enc.inverse_transform(clf.predict(to_predict))[0][0]
+    print("I think your dream destination is " + prediction)
+    is_correct = input("Am I correct? ")
+
+    if is_correct == "y":
+        print("Hooray!")
+        sys.exit(1)
+    elif is_correct == "n":
+        if diff != 0:
+            print("Oh dear. Let's continue...")
+        else:
+            print("Uh oh. I guess I don't know!")
+
+
+if __name__ == "__main__":
+    ask_user()
